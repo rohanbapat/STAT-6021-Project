953e48b46f4b75bb0a95a3e8d78c7ff231e6b6de
Rohan Bapat <rohanbapat2891@gmail.com>
2017-10-19 21:56:23 -0400

    Merged all input files

---
+++ STAT_6021_Project_v2.R
@@ -0,0 +1,44 @@
+library(RCurl)
+
+# Read input dataframes from Github
+colors_df <- read.csv(text =getURL('https://raw.githubusercontent.com/rohanbapat/STAT-6021-Project/master/colors.csv'))
+inventories_df <- read.csv(text =getURL('https://raw.githubusercontent.com/rohanbapat/STAT-6021-Project/master/inventories.csv'))
+inventoryparts_df <- read.csv(text =getURL('https://raw.githubusercontent.com/rohanbapat/STAT-6021-Project/master/inventory_parts.csv'))
+inventorysets_df <- read.csv(text =getURL('https://raw.githubusercontent.com/rohanbapat/STAT-6021-Project/master/inventory_sets.csv'))
+partcategories_df <- read.csv(text =getURL('https://raw.githubusercontent.com/rohanbapat/STAT-6021-Project/master/part_categories.csv'))
+parts_df <- read.csv(text =getURL('https://raw.githubusercontent.com/rohanbapat/STAT-6021-Project/master/parts.csv'))
+sets_df <- read.csv(text =getURL('https://raw.githubusercontent.com/rohanbapat/STAT-6021-Project/master/sets.csv'))
+themes_df <- read.csv(text =getURL('https://raw.githubusercontent.com/rohanbapat/STAT-6021-Project/master/themes.csv'))
+
+# Rename columns with same name but different definition
+colnames(partcategories_df)[colnames(partcategories_df) == "name"] <- "partcat_name"
+colnames(parts_df)[colnames(parts_df) == "name"] <- "part_name"
+colnames(colors_df)[colnames(colors_df) == "name"] <- "color_name"
+colnames(themes_df)[colnames(themes_df) == "name"] <- "theme_name"
+colnames(sets_df)[colnames(sets_df) == "name"] <- "set_name"
+colnames(inventoryparts_df)[colnames(inventoryparts_df) == "quantity"] <- "inventorypart_quantity"
+colnames(inventorysets_df)[colnames(inventorysets_df) == "quantity"] <- "inventorysets_quantity"
+
+# Merge all dataframes to master dataframe
+
+# 1. Consider inventoryparts as the base dataset (Since it is the largest in size)
+# Merge inventoryparts with colors on color_id <> id
+master_df <- merge(x = inventoryparts_df, y = colors_df,by.x = "color_id", by.y = "id")
+
+# 2. Merge parts and partcategories on part_cat_id <> id
+parts_partcat_merge <- merge(x = parts_df, y = partcategories_df, by.x = "part_cat_id", by.y = "id")
+
+# 3. Merge parts_partcat_merge with master on partnum
+master_df <- merge(master_df, parts_partcat_merge, by = "part_num", all.x = TRUE)
+
+# 4. Merge sets and themes on theme_id <> id
+sets_themes_merge <- merge(x = sets_df, y = themes_df, by.x = "theme_id", by.y = "id")
+
+# 5. Merge inventories with inventorysets on (id, set_num)<>(inventory_id, set_num)
+inventories_invset_merge <- merge(x = inventories_df, y = inventorysets_df, by.x = c("id","set_num"), by.y = c("inventory_id","set_num"), all = TRUE)
+
+# 6. Merge inventory sets with inventories_st_merge on set_num
+inventorysets_settheme_merge <- merge(inventories_invset_merge, sets_themes_merge, by = "set_num")
+
+# 7. Merge master with inventories_st_merge on inventory_id <> id
+master_df2 <- merge(x = master_df, y = inventorysets_settheme_merge, by.x = "inventory_id", by.y = "id", all = T)
